cc7e34afba7df7bab16bc9142d97b5a2ec61650d
yoon ho kang <yhkang@atozsoft.co.kr>
2020-10-15 16:42:14 +0900

    Updates

---
+++ precache-manifest.28f5594ef788f8f57a95e4eceada9f10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b74bda9f26a95d3761ad9d537dd869e",
+    "url": "/best-video/index.html"
+  },
+  {
+    "revision": "30b9c78f7bc952d86660",
+    "url": "/best-video/static/css/main.1fabf79e.chunk.css"
+  },
+  {
+    "revision": "64ee5b7c40c340c124ba",
+    "url": "/best-video/static/js/2.9b311772.chunk.js"
+  },
+  {
+    "revision": "3263eb4fa2a4ed54bb570011085a455d",
+    "url": "/best-video/static/js/2.9b311772.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30b9c78f7bc952d86660",
+    "url": "/best-video/static/js/main.9fc6ba52.chunk.js"
+  },
+  {
+    "revision": "8a4f8285a94ba5a20b8f",
+    "url": "/best-video/static/js/runtime-main.cdadb5f8.js"
+  }
+]);
